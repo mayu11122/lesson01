5a7ca5aa0a07ddd87870992c196cb7910b2e0d39
mayu11122 <63829415+mayu11122@users.noreply.github.com>
2020-06-23 23:00:22 +0900

    Update main.js

--- 4-2/main.js
+++ 4-2/main.js
@@ -1,6 +1,6 @@
 for (let i = 1; i <= 50; i++) {
     if (i % 3 === 0) {
-        console.log('WOW')
+        console.log('W')
     } else if (String(i).match('3')) {
         console.log('WOW')
     } else {
