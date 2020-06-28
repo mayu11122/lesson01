e2da0e0d3abfe87b895c2616fb7d09de5615c36a
mayu11122 <63829415+mayu11122@users.noreply.github.com>
2020-06-28 09:18:25 +0900

    Update main.js

--- 4-6/main.js
+++ 4-6/main.js
@@ -10,12 +10,12 @@ if (min + 45 < 60) {
     console.log(hour)
     console.log(min + 45)
 
-} else {
+} else if (min + 45 >= 60) {
     hours = hour + 1
     if (hours === 24 || hours > 24) {
         console.log(hours % 24)
     } else {
-        console.log(hours + 1)
+        console.log(hours)
     }
     console.log(min + 45 - 60)
 }
