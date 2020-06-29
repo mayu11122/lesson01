d9a7fc5ccff3b3773662cbf336d8c53cb5c6e476
渡邊真由 <mayu112ko@icloud.com>
2020-06-29 10:30:54 +0900

    5-3 修正しました！

--- 5-3/main.js
+++ 5-3/main.js
@@ -1,22 +1,20 @@
 const fs = require('fs')
-const jsonObject = JSON.parse(fs.readFileSync('./playdata.json', 'utf8'))
-const object2 = JSON.parse(fs.readFileSync('./songs.json', 'utf8'))
+const playdata = JSON.parse(fs.readFileSync('./playdata.json', 'utf8'))
+const songsfile = JSON.parse(fs.readFileSync('./songs.json', 'utf8'))
 
-jsonObject.data.sort(function (a, b) {
-    if (Number(a.count) < Number(b.count)) {
-        return 1
-    } else {
-        return -1
-    }
-
+playdata.data.sort(function (a, b) {
+    return b.count - a.count
 })
 
-for (let i = 0; i < object2.songs.length; i++) {
-    for (let j = 0; j < jsonObject.data.length; j++)
-        if (object2.songs[i].id === Number(jsonObject.data[j].id)) {
-            title = object2.songs[i].title
-            console.log('順位:' + i + '位' + title + '\t' + jsonObject.data[j].count)
+let title
+let count
+for (let i = 0; i < playdata.data.length; i++) {
+    for (let j = 0; j < songsfile.songs.length; j++) {
+        if (Number(playdata.data[i].id) === songsfile.songs[j].id) {
+            title = songsfile.songs[j].title
         }
-
+    }
+    count = playdata.data[i].count
+    console.log('順位:' + (i + 1) + '位\t' + title + '\t' + count)
 }
 
